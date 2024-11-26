783ee88bcd08e1641ddb5002de842558eb60ae92
Volodymyr Kurov <vokur@msn.com>
2024-11-26 16:23:47 +0000

    environment

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,6 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -114,14 +115,9 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",
-        "PORT": "5432",
-    }
+    "default": env.dj_db_url(
+        "DATABASE_URL", default="postgres://postgres:postgres@db/postgres"
+    ),
 }
 
 
