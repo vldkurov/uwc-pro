b257ef214bbed5b12b37981a067881453e0ec651
Volodymyr Kurov <vokur@msn.com>
2024-12-02 11:38:38 +0000

    context processor

--- locations/context_processors.py
+++ locations/context_processors.py
@@ -2,5 +2,9 @@ from .models import Division
 
 
 def divisions_context(request):
-    objects = Division.objects.first()
-    return {"division": objects}
+    first_division = Division.objects.first()
+
+    return {
+        "division": first_division,
+        "has_divisions": first_division is not None,  # Флаг наличия объектов Division
+    }
