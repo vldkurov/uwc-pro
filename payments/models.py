d5248d1f601af4b4ac79ed914aa57acd6729d0eb
Volodymyr Kurov <vokur@msn.com>
2024-12-13 19:05:20 +0000

    model update

--- payments/models.py
+++ payments/models.py
@@ -33,6 +33,7 @@ class Product(models.Model):
     type = models.CharField(max_length=50, default="SERVICE")
     category = models.CharField(max_length=50, default="DONATIONS")
     created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
